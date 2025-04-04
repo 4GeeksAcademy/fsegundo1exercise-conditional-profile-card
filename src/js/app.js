2f1dcd3cd841bb9bbf73fba9d623c68ee6743f62
Segundo Ferreira <segundo.ferreira28@gmail.com>
2025-04-04 18:30:31 +0000

    ultimos cambios, sobreescribi el proyecto por el error que me arrojaba la terminal

--- src/js/app.js
+++ src/js/app.js
@@ -28,22 +28,46 @@ function render(variables = {}) {
   // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  const newName =
-    // reset the website body with the new html output
-    (document.querySelector("#widget_content").innerHTML = `<div class="widget">
+  const newName = variables.name ? variables.name : "";
+  const lastName = variables.lastName ? variables.lastName : "";
+  const role = variables.role ? variables.role : "Web developer";
+  const city = variables.city ? variables.city : "Miami";
+  const country = variables.country ? variables.country : "USA";
+  const socialPosition = variables.socialMediaPosition
+    ? variables.socialMediaPosition
+    : "right";
+
+  // reset the website body with the new html output
+  document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
-            <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/4geeksacademy"><i class="fab fa-instagram"></i></a></li>
+          <h1>${newName} ${lastName}</h1>
+          <h2>${role}</h2>
+          <h3>${city}, ${country}</h3>
+          <ul class="position-${socialPosition}">
+          ${
+            variables.twitter
+              ? `<li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>`
+              : ""
+          }
+           ${
+             variables.github
+               ? `<li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>`
+               : ""
+           }
+            ${
+              variables.linkedin
+                ? `<li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>`
+                : ""
+            }
+            ${
+              variables.instagram
+                ? `<li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>`
+                : ""
+            }
           </ul>
         </div>
-    `);
+    `;
 }
 
 /**
